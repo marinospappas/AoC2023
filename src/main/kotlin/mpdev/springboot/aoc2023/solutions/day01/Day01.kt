527d1059841680d95dc6b6e167f2151e8f1a2426
marinos <marinosch.99981@gmail.com>
2023-12-01 07:05:30 +0100

    day 01

--- src/main/kotlin/mpdev/springboot/aoc2023/solutions/day01/Day01.kt
+++ src/main/kotlin/mpdev/springboot/aoc2023/solutions/day01/Day01.kt
@@ -2,6 +2,7 @@ package mpdev.springboot.aoc2023.solutions.day01
 
 import mpdev.springboot.aoc2023.model.PuzzlePartSolution
 import mpdev.springboot.aoc2023.solutions.PuzzleSolver
+import mpdev.springboot.aoc2023.utils.AocException
 import org.springframework.stereotype.Component
 import kotlin.system.measureNanoTime
 
@@ -29,16 +30,35 @@ class Day01: PuzzleSolver() {
 
     override fun solvePart1(): PuzzlePartSolution {
         val elapsed = measureNanoTime {
-
+            result = inputData.map { it.toCharArray().filter { d -> d.isDigit() } }
+                .sumOf { it.first().digitToInt() * 10 + it.last().digitToInt() }
         }
         return PuzzlePartSolution(1, result.toString(), elapsed/1000, "micro-sec")
     }
 
     override fun solvePart2(): PuzzlePartSolution {
+        val nums = listOf( "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
         val elapsed = measureNanoTime {
-
+            result = inputData
+                .sumOf { s -> findFirstNumber(s, nums) * 10 + findFirstNumber(s.reversed(), nums.map { n -> n.reversed() }) }
         }
         return PuzzlePartSolution(2, result.toString(), elapsed/1000, "micro-sec")
+    }
+
+    fun findFirstNumber(input: String, nums: List<String>): Int {
+        var s = input
+        while (s.isNotEmpty()) {
+            if (s.first().isDigit())
+                return s.first().digitToInt()
+            else {
+                for (n in nums) {
+                    if (s.startsWith(n))
+                        return nums.indexOf(n) + 1
+                }
+            }
+            s = s.substring(1, s.length)
+        }
+        throw AocException("no number was found in [$input]")
     }
 
 }
--- src/test/kotlin/mpdev/springboot/aoc2023/day01/Day01Test.kt
+++ src/test/kotlin/mpdev/springboot/aoc2023/day01/Day01Test.kt
@@ -6,8 +6,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
 class Day01Test {
 
@@ -32,24 +30,28 @@ class Day01Test {
     @Test
     @Order(2)
     fun `Reads Input ans sets Integer List`() {
-
-    }
-
-    @Test
-    @Order(3)
-    fun `Executes`() {
-
+        assertThat(puzzleSolver.inputData.size).isEqualTo(4)
     }
 
     @Test
     @Order(4)
     fun `Solves Part 1`() {
-        assertThat(puzzleSolver.solvePart1().result).isEqualTo("")
+        assertThat(puzzleSolver.solvePart1().result).isEqualTo("142")
     }
 
     @Test
     @Order(6)
     fun `Solves Part 2`() {
-        assertThat(puzzleSolver.solvePart2().result).isEqualTo("")
+        puzzleSolver.inputData = listOf(
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
+        )
+        puzzleSolver.initSolver()
+        assertThat(puzzleSolver.solvePart2().result).isEqualTo("281")
     }
 }
