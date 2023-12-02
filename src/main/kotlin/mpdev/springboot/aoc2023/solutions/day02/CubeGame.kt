07495649bb25fee6a65575a1d8380257af4970cc
marinos <marinosch.99981@gmail.com>
2023-12-02 11:24:47 +0100

    day 02 - refactoring of data set

--- src/main/kotlin/mpdev/springboot/aoc2023/solutions/day02/CubeGame.kt
+++ src/main/kotlin/mpdev/springboot/aoc2023/solutions/day02/CubeGame.kt
@@ -5,47 +5,53 @@ import mpdev.springboot.aoc2023.utils.AocException
 
 class CubeGame(input: List<String>) {
 
-    val games = mutableMapOf<Int, MutableSet<Pair<Int, Cube>>>()
+    val games = mutableMapOf<Int, MutableSet<MutableSet<Pair<Int, Cube>>>>()
     val cubesSet = setOf(Pair(12, red), Pair(13, green), Pair(14, blue))
 
     init {
-        processInput(input)
+        processInput(input, games)
     }
 
-    fun isGameValid(game: Set<Pair<Int, Cube>>) =
-        game.all { cube -> cube.first <= cubesSet.first { it.second == cube.second }.first }
+    fun isGameValid(game: Set<Set<Pair<Int, Cube>>>) =
+        game.flatten().all { cube -> cube.first <= cubesSet.first { it.second == cube.second }.first }
 
-    fun minCubesForGame(game: Set<Pair<Int, Cube>>) =
-        Cube.values().associateWith { c -> game.filter { it.second == c }.maxOf { it.first } }
+    fun minCubesForGame(game: Set<Set<Pair<Int, Cube>>>) =
+        Cube.values().associateWith { c -> game.flatten().filter { it.second == c }.maxOf { it.first } }
             .entries.map { e -> Pair(e.value, e.key) }.toSet()
 
     fun powerOfSet(cubes: Set<Pair<Int, Cube>>) =
         cubes.map{ it.first }.fold(1) { acc, x -> acc * x }
 
-    private fun processInput(input: List<String>) {
-        input.forEach { line ->
-            // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-            val match = Regex("""Game (\d+): (.+)""").find(line)
-            try {
-                val (id, remaining) = match!!.destructured
-                processGame(id.toInt(), remaining)
-            } catch (e: Exception) {
-                throw AocException("bad input line [$line], ${e.message}")
+
+    companion object {
+        private fun processInput(input: List<String>, games: MutableMap<Int, MutableSet<MutableSet<Pair<Int, Cube>>>>) {
+            input.forEach { line ->
+                // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                val match = Regex("""Game (\d+): (.+)""").find(line)
+                try {
+                    val (id, remaining) = match!!.destructured
+                    games[id.toInt()] = mutableSetOf()
+                    processGame(id.toInt(), remaining, games)
+                } catch (e: Exception) {
+                    throw AocException("bad input line [$line], ${e.message}")
+                }
             }
         }
-    }
 
-    private fun processGame(id: Int, input: String) {
-        input.split(Regex("; ")).forEach { g ->
-            // 1 red, 2 green, 6 blue
-            g.split(Regex(", ")).forEach { c ->
-                val match = Regex("""(\d+) ([a-z]+)""").find(c)
-                try {
-                    val (count, cube) = match!!.destructured
-                    games.getOrPut(id) { mutableSetOf() }.add(Pair(count.toInt(), Cube.valueOf(cube)))
-                } catch (e: Exception) {
-                    throw AocException("bad input for cubes $input")
+        private fun processGame(id: Int, input: String, games: MutableMap<Int, MutableSet<MutableSet<Pair<Int, Cube>>>>) {
+            input.split(Regex("; ")).forEach { g ->
+                // 1 red, 2 green, 6 blue
+                val cubeSet = mutableSetOf<Pair<Int, Cube>>()
+                g.split(Regex(", ")).forEach { c ->
+                    val match = Regex("""(\d+) ([a-z]+)""").find(c)
+                    try {
+                        val (count, cube) = match!!.destructured
+                        cubeSet.add(Pair(count.toInt(), Cube.valueOf(cube)))
+                    } catch (e: Exception) {
+                        throw AocException("bad input for cubes $input")
+                    }
                 }
+                games[id]!!.add(cubeSet)
             }
         }
     }
