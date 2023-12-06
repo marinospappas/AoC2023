ffb51bae4e087bff5494da6a01eb8ce61e56a250
marinos <marinosch.99981@gmail.com>
2023-12-06 08:30:05 +0100

    quadratic equation util

--- src/main/kotlin/mpdev/springboot/aoc2023/solutions/day06/Day06.kt
+++ src/main/kotlin/mpdev/springboot/aoc2023/solutions/day06/Day06.kt
@@ -3,7 +3,7 @@ package mpdev.springboot.aoc2023.solutions.day06
 import mpdev.springboot.aoc2023.model.PuzzlePartSolution
 import mpdev.springboot.aoc2023.solutions.PuzzleSolver
 import org.springframework.stereotype.Component
-import kotlin.system.measureTimeMillis
+import kotlin.system.measureNanoTime
 
 @Component
 class Day06: PuzzleSolver() {
@@ -20,23 +20,23 @@ class Day06: PuzzleSolver() {
     lateinit var boatRace: BoatRace
 
     override fun initSolver(): Pair<Long,String> {
-        val elapsed = measureTimeMillis {
+        val elapsed = measureNanoTime {
             boatRace = BoatRace(inputData)
         }
-        return Pair(elapsed, "milli-sec")
+        return Pair(elapsed/1000, "micro-sec")
     }
 
     override fun solvePart1(): PuzzlePartSolution {
-        val elapsed = measureTimeMillis {
+        val elapsed = measureNanoTime {
             result = boatRace.races.map { boatRace.minMaxChargeTime(it) }.map { it.second - it.first + 1 }.reduce(Long::times)
         }
-        return PuzzlePartSolution(1, result.toString(), elapsed, "milli-sec")
+        return PuzzlePartSolution(1, result.toString(), elapsed/1000, "micro-sec")
     }
 
     override fun solvePart2(): PuzzlePartSolution {
-        val elapsed = measureTimeMillis {
+        val elapsed = measureNanoTime {
             result = boatRace.minMaxChargeTime(boatRace.setupLongRace()).let { it.second - it.first + 1 }
         }
-        return PuzzlePartSolution(2, result.toString(), elapsed, "milli-sec")
+        return PuzzlePartSolution(2, result.toString(), elapsed/1000, "micro-sec")
     }
 }
--- src/main/kotlin/mpdev/springboot/aoc2023/utils/QuadraticEq.kt
+++ src/main/kotlin/mpdev/springboot/aoc2023/utils/QuadraticEq.kt
@@ -11,16 +11,16 @@ class QuadraticEq {
         fun solve(a: Long, b: Long, c: Long): Pair<Double,Double> {
             // b squared - 4 a c
             val d = sqrt((b * b - 4 * a * c).toDouble())
-            val x1 = (-b - d) / 2
-            val x2 = (-b + d) / 2
+            val x1 = (-b - d) / (2 * a)
+            val x2 = (-b + d) / (2 * a)
             return Pair(x1, x2)
         }
 
         fun solve(a: Double, b: Double, c: Double): Pair<Double,Double> {
             // b squared - 4 a c
             val d = sqrt(b * b - 4 * a * c)
-            val x1 = (-b - d) / 2
-            val x2 = (-b + d) / 2
+            val x1 = (-b - d) / (2 * a)
+            val x2 = (-b + d) / (2 * a)
             return Pair(x1, x2)
         }
     }
