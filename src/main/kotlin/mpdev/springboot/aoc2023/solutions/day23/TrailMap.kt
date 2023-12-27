429194d0674a191a6f66a6463c3f24b14ff3b2ae
marinos <marinosch.99981@gmail.com>
2023-12-27 17:06:43 +0100

    day 23 refactored to use the new SGraph class in both part1 and part2 for efficiancy

--- src/main/kotlin/mpdev/springboot/aoc2023/solutions/day23/TrailMap.kt
+++ src/main/kotlin/mpdev/springboot/aoc2023/solutions/day23/TrailMap.kt
@@ -3,7 +3,6 @@ package mpdev.springboot.aoc2023.solutions.day23
 import mpdev.springboot.aoc2023.solutions.day23.HikePath.*
 import mpdev.springboot.aoc2023.utils.*
 
-// TODO: refactor this solution to use the new SGraph class and DFS also for part 1 for consistency and efficiency
 class TrailMap(input: List<String>) {
 
     var debug = false
@@ -14,27 +13,6 @@ class TrailMap(input: List<String>) {
     val end: Point = grid.getDataPoints().filter { e -> e.key.y == maxy && e.value == PATH }
         .keys.first()
 
-    private val graph = Graph(this::getNextPathPts).also { g -> grid.getDataPoints().keys.forEach { g.addNode(it) } }
-
-    private fun getNextPathPts(current: Point, ignore: DijkstraPathMap<Point>?): List<GraphNode<Point>> {
-        val nextPts = mutableListOf<Point>()
-        val curPathPt = grid.getDataPoint(current)!!
-        if (curPathPt in SLOPE_1 .. SLOPE_4) {
-            nextPts.add(current + curPathPt.next)
-        }
-        else {
-            nextPts.addAll(
-                current.adjacentCardinal().filter { grid.getDataPoint(it) != null && grid.getDataPoint(it)!! in PATH .. SLOPE_4 }
-            )
-        }
-        return nextPts.map { graph[it] }
-    }
-
-    fun findAllPaths(): List<List<Point>> {
-        val allPaths = Bfs<Point>().allPaths(graph[start], graph[end])
-        return allPaths.map { l -> l.map { v -> v.getId() } }
-    }
-
     fun findMaxPath1(): Int {
         return initGraph(1).longestPathDfs(start, end)
     }
@@ -43,7 +21,14 @@ class TrailMap(input: List<String>) {
         return SGraph(reduceGraph(initGraph(2)).nodes).longestPathDfs(start, end)
     }
 
-    fun reduceGraph(graph: SGraph<Point>): SGraph<Point> {
+    /**
+     * reduce the graph
+     * by combining all the "straight" path segments to one connection
+     * This is done by repeatedly taking all the nodes that have only 2 connected nodes (i.e. either right/left or up/down)
+     * and taking these nodes out and connecting the node before with the node after and also adjusting the number of steps between
+     * This way only nodes with 3 or 4 connections are left plus the start and the end
+     */
+    private fun reduceGraph(graph: SGraph<Point>): SGraph<Point> {
         val adjacencyMap = graph.nodes.toMutableMap()
         adjacencyMap.keys.toList().forEach { key ->
             adjacencyMap[key]?.takeIf { it.size == 2 }?.let { neighbors ->
@@ -52,7 +37,7 @@ class TrailMap(input: List<String>) {
                 val totalSteps = neighbors[left]!! + neighbors[right]!!
                 adjacencyMap.getOrPut(left) { mutableMapOf() }.merge(right, totalSteps, ::maxOf)
                 adjacencyMap.getOrPut(right) { mutableMapOf() }.merge(left, totalSteps, ::maxOf)
-                listOf(left, right).forEach { adjacencyMap[it]?.remove(key) }
+                for (p in setOf(left,right)) adjacencyMap[p]?.remove(key)
                 adjacencyMap.remove(key)
             }
         }
@@ -61,23 +46,29 @@ class TrailMap(input: List<String>) {
 
     fun initGraph(part1or2: Int): SGraph<Point> {
         val g = SGraph<Point>()
-        if (part1or2 == 1)
-            grid.getDataPoints().filter { e -> e.value == PATH }.keys.forEach { p ->
-                val adjacent =
-                    p.adjacentCardinal().filter { adj ->
-                        grid.isInsideGrid(adj) && grid.getDataPoint(adj) == PATH
-                    }
-                g.addNode(p, adjacent.associateWith { 1 })
-            }
-        else
-            grid.getDataPoints().filter { e -> e.value != FORREST }.keys.forEach { p ->
-                val adjacent =
-                    p.adjacentCardinal().filter { adj ->
-                        grid.isInsideGrid(adj) && grid.getDataPoint(adj) != FORREST
-                    }
-                g.addNode(p, adjacent.associateWith { 1 })
-            }
+        grid.getDataPoints().filter { e -> e.value != FORREST }.keys.forEach { p ->
+            g.addNode(p, getAdjacent(p, part1or2))
+        }
         return g
+    }
+
+    private fun getAdjacent(point: Point, part1Or2: Int): Set<Point> {
+        val adjacent = mutableSetOf<Point>()
+        val datum = grid.getDataPoint(point)
+        if (part1Or2 == 1)
+            when (grid.getDataPoint(point)) {
+                PATH -> adjacent.addAll(point.adjacentCardinal())
+                SLOPE_1,SLOPE_2,SLOPE_3,SLOPE_4 -> adjacent.add(point + GridUtils.Direction.of(datum!!.value).increment)
+                else -> {}
+            }
+        else // part 2
+            adjacent.addAll(point.adjacentCardinal())
+        adjacent.removeAll { adj -> grid.getDataPoint(adj) == FORREST }
+        if (point == start)
+            adjacent.removeAll { it.y < 0 }
+        else if (point == end)
+            adjacent.clear()
+        return adjacent
     }
 }
 
